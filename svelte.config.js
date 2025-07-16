1103b4a77dcb997af3156f442ea0d411698d666b
Abdulhaq Adetunji Salako <adetunjisalako@gmail.com>
2025-07-16 13:57:10 +0200

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 			strict: true
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/my-profile-site'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
