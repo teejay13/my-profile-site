449909304180a1bb24aaaa06c378e8c5e7f9e85c
Teejay <abdulhaq.salako@uni-rostock.de>
2025-07-16 14:12:25 +0200

    test page on deploy

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<main class="flex flex-col">
+    <h1 class="text-3xl font-bold underline">
+        Hello world!
+    </h1>
+    <h1 class="text-3xl font-bold">
+        Teejay Personal Site
+    </h1>
+</main>
